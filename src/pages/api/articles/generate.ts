21dbf9b3b91e3005ae17963f51b1c9f674b40936
jhawkins11 <josiah.c.hawkins@gmail.com>
2023-11-15 07:20:36 -0600

    fix duplicate slug error

--- src/pages/api/articles/generate.ts
+++ src/pages/api/articles/generate.ts
@@ -24,7 +24,8 @@ export default async function handler(
       Format: 1 h1/2+ h2s
       `
       : `
-        Generate MDL Article in a format that works with react-markdown: ${topic}
+        Generate MDL Article in a format that works with react-markdown.
+        Article Title: ${topic}
         Format: 1 h1/2+ h2s
         `
 
@@ -37,9 +38,9 @@ export default async function handler(
     // create a unique slug for the article
     // if the article is a child article, we don't need to create a slug
     let slug = ''
-    if (!parentid) {
-      slug = await createUniqueSlug({ title, model: ArticleModel })
-    }
+
+    slug = await createUniqueSlug({ title, model: ArticleModel })
+
     // save article to mongoDb
     const created = await ArticleModel.create({
       parentid: parentid || '',
@@ -50,6 +51,7 @@ export default async function handler(
     })
     return res.status(200).json(created as any)
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ message: 'Error generating article' })
   }
 }
