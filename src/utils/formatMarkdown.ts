8b828d6f0b7787a0fc669196dd552fbf0233f4d3
jhawkins11 <josiah.c.hawkins@gmail.com>
2024-04-08 10:52:31 -0500

    fix improper markdown formatting

--- src/utils/formatMarkdown.ts
+++ src/utils/formatMarkdown.ts
@@ -4,14 +4,22 @@ const formatMarkdown = (content: string) => {
   // it will not render the markdown properly
   const lines = content.split('\n')
   let isCodeBlock = false
-  const trimmedLines = lines.map((line) => {
+  const trimmedLines = lines.map((line, index) => {
     // if line is within a multiline code block, don't trim it
-    if (line.startsWith('```')) {
+    if (line.startsWith('```markdown')) {
       isCodeBlock = !isCodeBlock
+      if (index === 0) {
+        // remove the starting code block
+        return line.replace('```markdown', '')
+      }
       return line
     }
     if (isCodeBlock && line.endsWith('```')) {
       isCodeBlock = !isCodeBlock
+      if (index === lines.length - 1) {
+        // remove the ending code block
+        return line.replace('```', '')
+      }
       return line
     }
     if (isCodeBlock) {
