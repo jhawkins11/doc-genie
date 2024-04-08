3d7c435b6588b78e0fcee91c0472cb808c6714d2
jhawkins11 <josiah.c.hawkins@gmail.com>
2024-04-08 12:27:03 -0500

    Refactor createUniqueSlug function to handle duplicate slugs

--- src/utils/createUniqueSlug.ts
+++ src/utils/createUniqueSlug.ts
@@ -9,16 +9,16 @@ const createUniqueSlug = async ({
 }): Promise<string> => {
   const slug = title.toLowerCase().replace(/ /g, '-')
 
-  // find all existing rows with the same slug
+  // find all existing rows with the same base slug
   const existing = await model.find({
-    slug,
+    slug: { $regex: new RegExp(`^${slug}(-[0-9]*)?$`, 'i') },
   })
   // if there are existing rows with the same slug, add a number to the end of the slug
   // the number will be the number of existing articles with the same slug
   if (existing.length > 0) {
-    const newSlug = `${slug}-${existing.length}`
-    // update the article with the new slug
-    return newSlug
+    const lastSlug = existing[existing.length - 1].slug
+    const lastSlugNumber = parseInt(lastSlug.split('-').pop())
+    return `${slug}-${lastSlugNumber + 1}`
   }
   return slug
 }
